ecbc624a123d66de59c2144ba7fecd50877106a0
Omri Zaiman <omri162004@gmail.com>
2021-03-23 18:23:18 +0200

    uploaded first version of the web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AngularFireModule} from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAKdcjgoYN6Ye822cIKZpnlluJGzqRJspk",
+      authDomain: "loginproject-5caee.firebaseapp.com",
+      projectId: "loginproject-5caee",
+      storageBucket: "loginproject-5caee.appspot.com",
+      messagingSenderId: "256709267053",
+      appId: "1:256709267053:web:4c94c879b54c5253777964",
+      measurementId: "G-XDJJ1EHMVW"
+    })
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
