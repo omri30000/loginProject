7198cd019db6cd69671835b57ee0689538e0d309
Omri Zaiman <omri162004@gmail.com>
2021-03-24 22:20:51 +0200

    added material design to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AngularFireModule} from '@angular/fire'
+import {AngularFireModule} from '@angular/fire';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FirebaseService } from './services/firebase.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,14 +14,15 @@ import { FirebaseService } from './services/firebase.service';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyAKdcjgoYN6Ye822cIKZpnlluJGzqRJspk",
-      authDomain: "loginproject-5caee.firebaseapp.com",
-      projectId: "loginproject-5caee",
-      storageBucket: "loginproject-5caee.appspot.com",
-      messagingSenderId: "256709267053",
-      appId: "1:256709267053:web:4c94c879b54c5253777964",
-      measurementId: "G-XDJJ1EHMVW"
-    })
+      apiKey: 'AIzaSyAKdcjgoYN6Ye822cIKZpnlluJGzqRJspk',
+      authDomain: 'loginproject-5caee.firebaseapp.com',
+      projectId: 'loginproject-5caee',
+      storageBucket: 'loginproject-5caee.appspot.com',
+      messagingSenderId: '256709267053',
+      appId: '1:256709267053:web:4c94c879b54c5253777964',
+      measurementId: 'G-XDJJ1EHMVW'
+    }),
+    BrowserAnimationsModule
   ],
   providers: [FirebaseService],
   bootstrap: [AppComponent]
